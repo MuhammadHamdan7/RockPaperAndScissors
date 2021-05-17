5afdbeb8d9f4f859974f9a9a5e19575ea32ed366
MuhammadHamdan7 <muhamad.mh7@gmail.com>
2021-05-17 21:50:53 +0300

    Add project files.

---
+++ RockPaperAndScissors/Helpers/Hand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockPaperAndScissors.Helpers
+{
+    public class Hand
+    {
+        public OptionRPS Selection { get; set; }
+        public OptionRPS WinAgainst { get; set; }
+        public OptionRPS LosesAgainst { get; set; }
+
+        public String Image { get; set; } 
+
+        public GameStatus PlayAgainst(Hand opponentHand)
+        {
+            if (Selection == opponentHand.Selection)
+            {
+                return GameStatus.Draw;
+            }
+
+            if (LosesAgainst == opponentHand.Selection)
+            {
+                return GameStatus.Loss;
+            }
+                return GameStatus.Victory;
+        }
+    }
+}
